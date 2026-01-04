e7e49e381277b9272c7092c63bfeada15305e57f
Krishna Narayan Singh <krishnanarayansingh65@gmail.com>
2026-01-04 15:30:44 +0530

    commit

--- project_first1/src/components/header.jsx
+++ project_first1/src/components/header.jsx
@@ -2,7 +2,7 @@ export default function Header(){
     return(
         <header>
             <img src="src\assets\chef.png" alt="chef" />
-            <p>Chef Claude</p>
+            <h1>Chef Claude</h1>
         </header>
 
     )
---
+++ project_first1/src/components/main.jsx
@@ -0,0 +1,10 @@
+export default function Main(){
+    return (
+        <main>
+            <form>
+                <input type="text" placeholder="e.g. Oregano" aria-label="add ingredients"/> 
+                <button>+ Add  Ingredients</button>   
+            </form> 
+        </main>
+    )
+}
